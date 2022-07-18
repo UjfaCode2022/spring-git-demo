24c687d7e8e03bbdb06a3dc43ae845ba8285ff10
DESKTOP-V0073ID\Java3 <aleksandar.ujfalusi@codeedu.rs>
2022-07-18 20:38:49 +0200

    initialized spring boot project

---
+++ src/main/java/rs/edu/code/springgitdemo/SpringGitDemoApplication.java
@@ -0,0 +1,13 @@
+package rs.edu.code.springgitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/edu/code/springgitdemo/SpringGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.edu.code.springgitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
